fa70e4228462296dc79c7d3245ace71452ddb471
Luke Karrys <lukekarrys@gmail.com>
2013-11-29 10:57:26 -0700

    add parallax horizontal and vertical properties to enable non-calculated parallax offsets

--- js/reveal.js
+++ js/reveal.js
@@ -136,6 +136,10 @@
 			// Parallax background size
 			parallaxBackgroundSize: '', // CSS syntax, e.g. "3000px 2000px"
 
+			// Amount to move parallax background (horizontal and vertical) on slide change
+			parallaxBackgroundHorizontal: '', //  Number, e.g. 100
+			parallaxBackgroundVertical: '', // Number, e.g. 10
+
 			// Number of slides away from the current that are visible
 			viewDistance: 3,
 
@@ -2530,13 +2534,29 @@
 				backgroundHeight = parseInt( backgroundSize[1], 10 );
 			}
 
-			var slideWidth = dom.background.offsetWidth;
-			var horizontalSlideCount = horizontalSlides.length;
-			var horizontalOffset = -( backgroundWidth - slideWidth ) / ( horizontalSlideCount-1 ) * indexh;
+			var slideWidth = dom.background.offsetWidth,
+				horizontalSlideCount = horizontalSlides.length,
+				horizontalOffsetMultiplier, horizontalOffset;
 
-			var slideHeight = dom.background.offsetHeight;
-			var verticalSlideCount = verticalSlides.length;
-			var verticalOffset = verticalSlideCount > 1 ? -( backgroundHeight - slideHeight ) / ( verticalSlideCount-1 ) * indexv : 0;
+			if (typeof config.parallaxBackgroundHorizontal === 'number') {
+				horizontalOffsetMultiplier = config.parallaxBackgroundHorizontal;
+			} else {
+				horizontalOffsetMultiplier = ( backgroundWidth - slideWidth ) / ( horizontalSlideCount-1 );
+			}
+
+			horizontalOffset = horizontalOffsetMultiplier * indexh * -1;
+
+			var slideHeight = dom.background.offsetHeight,
+				verticalSlideCount = verticalSlides.length,
+				verticalOffsetMultiplier, verticalOffset;
+
+			if (typeof config.parallaxBackgroundVertical === 'number') {
+				verticalOffsetMultiplier = config.parallaxBackgroundVertical;
+			} else {
+				verticalOffsetMultiplier = ( backgroundHeight - slideHeight ) / ( verticalSlideCount-1 );
+			}
+
+			verticalOffset = verticalSlideCount > 0 ?  verticalOffsetMultiplier * indexv * -1 : 0;
 
 			dom.background.style.backgroundPosition = horizontalOffset + 'px ' + verticalOffset + 'px';
 
